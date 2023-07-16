fe905516ad908335ea4d10596ca0c017c19841c8
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-07-15 23:47:17 -0300

    att

--- src/controllers/MealsController.js
+++ src/controllers/MealsController.js
@@ -1,83 +1,152 @@
-import { AppError } from '../utils/AppError.js'
 import { connection as knex } from '../database/knex/index.js'
+import { DiskStorage } from '../providers/DiskStorage.js'
 
 export class MealsController {
   async create(request, response) {
     const { title, description, category, price, ingredients } = request.body
 
-    const [meal_id] = await knex("meals").insert({
-      title,
-      description,
-      category,
-      price
-    })
+    const mealImage = request.file
 
-    const ingredientsInsert = ingredients.map(name => {
-      return {
-        meal_id,
-        name
+    try {
+      let filename = null
+  
+      if (mealImage) {
+        const diskStorage = new DiskStorage()
+        filename = await diskStorage.saveFile(mealImage.filename)
       }
-    })
 
-    await knex("ingredients").insert(ingredientsInsert)
+      const [meal_id] = await knex('meals').insert({
+        image: filename,
+        title,
+        description,
+        category,
+        price
+      })
 
-    return response.status(201).json()
+      const hasOnlyOneIngredient = typeof(ingredients) === "string";
+
+      let ingredientsInsert
+
+      if (hasOnlyOneIngredient) {
+        ingredientsInsert = {
+          name: ingredients,
+          meal_id
+        }
+      } else if (ingredients.length > 1) {
+        ingredientsInsert = ingredients.map(ingredient => {
+          return {
+            name : ingredient,
+            meal_id
+          }
+        })
+      } else {
+        return 
+      }
+
+      await knex("ingredients").insert(ingredientsInsert)
+
+      return response.status(201).json()
+    } catch (error) {
+      console.log('Ocorreu um erro ao processar a imagem', error)
+    }
   }
 
   async update(request, response) {
     const { title, description, category, price, ingredients } = request.body
-
     const { id } = request.params
+  
+    const mealImage = request.file
+  
+    try {
+      let filename = null
+  
+      if (mealImage) {
+        const diskStorage = new DiskStorage()
+        filename = await diskStorage.saveFile(mealImage.filename)
+      }
+  
+      if (!filename && !title && !description && !category && !price) {
+        return response.status(200).json()
+      }
 
-    const mealsList = await knex('meals').where('id', id)
-    const meal = mealsList[0]
+      const updateData = {}
+  
+      if (filename) {
+        updateData.image = filename
+      }
+  
+      if (title) {
+        updateData.title = title
+      }
+  
+      if (description) {
+        updateData.description = description
+      }
+  
+      if (category) {
+        updateData.category = category
+      }
+  
+      if (price) {
+        updateData.price = price
+      }
+  
+      await knex('meals')
+        .where('id', id)
+        .update(updateData)
 
-    if(!meal) {
-      throw new AppError("Prato não encontrado!")
+      await knex('ingredients')
+        .where('meal_id', id)
+        .del()
+  
+      const hasOnlyOneIngredient = typeof ingredients === 'string'
+  
+      let ingredientsInsert
+  
+      if (hasOnlyOneIngredient) {
+        ingredientsInsert = {
+          name: ingredients,
+          meal_id: id
+        }
+      } else if (ingredients.length > 1) {
+        ingredientsInsert = ingredients.map(ingredient => {
+          return {
+            name: ingredient,
+            meal_id: id
+          }
+        })
+      } else {
+        return
+      }
+
+      await knex('ingredients').insert(ingredientsInsert)
+  
+      return response.status(200).json()
+    } catch (error) {
+      return response.status(500).json({ error: 'Erro ao atualizar o prato' })
     }
-
-    meal.title = title ?? meal.title
-    meal.description = description ?? meal.description
-    meal.category = category ?? meal.category
-    meal.price = price ?? meal.price
-
-    await knex('meals').where('id', id).update({
-      title,
-      description,
-      category,
-      updated_at: new Date().toISOString().slice(0, 19).replace('T', ' ')
-    })
-
-    const ingredientsUpdate = ingredients.map(name => ({
-      name,
-      meal_id: meal.id
-    }))
-
-    await knex("ingredients").where({meal_id: id}).delete();
-    await knex("ingredients").insert(ingredientsUpdate);
-
-    return response.status(200).json()
-
   }
-
+  
   async delete(request, response) {
     const { id } = request.params
 
-    await knex("meals").where({ id }).delete()
+    await knex('meals').where({ id }).delete()
 
     return response.json()
   }
 
   async show(request, response) {
-    const { id } = request.params;
+    const { id } = request.params
 
-    const meal = await knex("meals").where({ id }).first();
-    const ingredients = await knex("ingredients").where({ meal_id: id}).orderBy("name");
+    const meal = await knex('meals').where({ id }).first()
+    const ingredients = await knex('ingredients')
+      .where({ meal_id: id })
+      .orderBy('name')
 
     return response.json({
       ...meal,
       ingredients
-    });
+    })
   }
 
   async index(request, response) {
@@ -85,41 +154,43 @@ export class MealsController {
 
     let meals
 
-    if(ingredients) {
-      const filterIngredients = ingredients.split(',').map(ingredient => ingredient.trim())
+    if (ingredients) {
+      const filterIngredients = ingredients
+        .split(',')
+        .map(ingredient => ingredient.trim())
 
-      meals = await knex("ingredients")
+      meals = await knex('ingredients')
         .select([
-          "meals.id",
-          "meals.title",
-          "meals.description",
-          "meals.category",
-          "meals.price"
+          'meals.id',
+          'meals.title',
+          'meals.description',
+          'meals.category',
+          'meals.price'
         ])
-        .whereLike("meals.title", `%${title}%`)
-        .whereIn("name", filterIngredients)
-        .innerJoin("meals", "meals.id", "ingredients.meal_id")
-        .groupBy("meals.id")
-        .orderBy("meals.title")
-
+        .whereLike('meals.title', `%${title}%`)
+        .whereIn('name', filterIngredients)
+        .innerJoin('meals', 'meals.id', 'ingredients.meal_id')
+        .groupBy('meals.id')
+        .orderBy('meals.title')
     } else {
-        meals = await knex("meals")
-          .whereLike("title", `%${title}%`)
-          .orderBy("title")	
+      meals = await knex('meals')
+        .whereLike('title', `%${title}%`)
+        .orderBy('title')
     }
-    
-    const mealsIngredients = await knex("ingredients")
+
+    const mealsIngredients = await knex('ingredients')
 
     const mealsWithIngredients = meals.map(meal => {
-      const mealIngredients = mealsIngredients.filter(ingredient => ingredient.meal_id === meal.id)
+      const mealIngredients = mealsIngredients.filter(
+        ingredient => ingredient.meal_id === meal.id
+      )
 
       return {
         ...meal,
-       ingredients: mealIngredients
+        ingredients: mealIngredients
       }
     })
 
     return response.json(mealsWithIngredients)
   }
-
 }
--- src/routes/meals.routes.js
+++ src/routes/meals.routes.js
@@ -1,12 +1,21 @@
 import { Router } from "express"
+import multer from 'multer'
+import uploadConfig from '../configs/upload.js'
 import { MealsController } from "../controllers/MealsController.js"
+import { MealsImageController } from "../controllers/MealsImageController.js"
+import { ensureAuthenticatedAdmin } from "../middleware/ensureAuthenticatedAdmin.js"
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js"
 
 export const mealsRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
+
+mealsRoutes.use(ensureAuthenticated)
 
 const mealsController = new MealsController()
+const mealsImageController = new MealsImageController()
 
-mealsRoutes.post("/", mealsController.create)
-mealsRoutes.put("/:id", mealsController.update)
-mealsRoutes.delete("/:id", mealsController.delete)
+mealsRoutes.post("/", ensureAuthenticatedAdmin, upload.single("image"),mealsController.create)
+mealsRoutes.put("/:id", ensureAuthenticatedAdmin, upload.single("image"), mealsController.update)
+mealsRoutes.delete("/:id", ensureAuthenticatedAdmin, mealsController.delete)
 mealsRoutes.get("/:id", mealsController.show)
 mealsRoutes.get("/", mealsController.index)
